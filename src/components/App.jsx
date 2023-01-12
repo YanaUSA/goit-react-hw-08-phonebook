265d93d5148029801d265ffb6646d73f13292106
YanaUSA <usatiukiana@gmail.com>
2023-01-12 19:50:17 +0200

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,15 @@ import { Container, Title, ContactsTitle } from './App.styled';
 import { useState, useEffect } from 'react';
 
 export const App = () => {
+  const contactsBase = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
   const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(window.localStorage.getItem('contacts')) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? contactsBase;
   });
   const [filter, setFilter] = useState('');
 
