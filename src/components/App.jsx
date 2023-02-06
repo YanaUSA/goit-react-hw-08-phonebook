674c03077f3a5dce3612f792af92429618090b84
YanaUSA <usatiukiana@gmail.com>
2023-02-06 14:57:29 +0200

    add persistor to configureStore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,4 @@
 import { nanoid } from 'nanoid';
-import { useState, useEffect } from 'react';
-
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import { Container, Title, ContactsTitle } from './App.styled';
-
 import { useDispatch, useSelector } from 'react-redux';
 import {
     addedContact,
@@ -13,19 +6,15 @@ import {
     filteredContact,
 } from '../redux/contacts/contactsSlice';
 import { contactsData, filteredData } from '../redux/selectors/selectors';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Container, Title, ContactsTitle } from './App.styled';
 
 export const App = () => {
     const contacts_store = useSelector(contactsData);
     const filterByName = useSelector(filteredData);
     const dispatch = useDispatch();
-
-    // const [contacts, setContacts] = useState(() => {
-    //     return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
-    // });
-
-    // useEffect(() => {
-    //     window.localStorage.setItem('contacts', JSON.stringify(contacts));
-    // }, [contacts]);
 
     const addContact = obj => {
         const newContact = {
@@ -59,13 +48,9 @@ export const App = () => {
     return (
         <Container>
             <Title>Phonebook</Title>
-
             <ContactForm addContact={addContact} contacts={contacts_store} />
-
             <ContactsTitle>Contacts</ContactsTitle>
-
             <Filter addFilterValue={filteredName} />
-
             <ContactList
                 contacts={getFilteredContact()}
                 deleteContact={deleteContact}
--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const contactsList = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const contactsList = [
+//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 export const contactsSlice = createSlice({
     name: 'contacts',
     initialState: {
-        contacts: contactsList,
+        contacts: [],
         filter: '',
     },
     reducers: {
