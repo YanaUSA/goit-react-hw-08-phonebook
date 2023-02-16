141cb8628055c4795b67f1336a28518bda379b7a
YanaUSA <usatiukiana@gmail.com>
2023-02-16 13:34:39 +0200

    add persistor draft

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import './index.css';
 import { store } from './redux/store/store';
 import { Provider } from 'react-redux';
 
+// import { store, persistor } from './redux/store/store';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
+            {/* <PersistGate loading={null} persistor={persistor}> */}
             <App />
+            {/* </PersistGate> */}
         </Provider>
     </React.StrictMode>
 );
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -4,3 +4,43 @@ import rootReducer from '../contacts/contactsSlice';
 export const store = configureStore({
     reducer: rootReducer,
 });
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import {
+//     persistStore,
+//     persistReducer,
+//     FLUSH,
+//     REHYDRATE,
+//     PAUSE,
+//     PERSIST,
+//     PURGE,
+//     REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import rootReducer from '../contacts/contactsSlice';
+
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+//     whitelist: ['contacts'],
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//     reducer: persistedReducer,
+//     middleware: getDefaultMiddleware =>
+//         getDefaultMiddleware({
+//             serializableCheck: {
+//                 ignoredActions: [
+//                     FLUSH,
+//                     REHYDRATE,
+//                     PAUSE,
+//                     PERSIST,
+//                     PURGE,
+//                     REGISTER,
+//                 ],
+//             },
+//         }),
+// });
+// export const persistor = persistStore(store);
